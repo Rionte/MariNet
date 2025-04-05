86c09a93635fcd68c226054048e4a928fd577065
Hassan-Ali223 <hassanaliwork3@gmail.com>
2025-04-04 23:06:07 -0400

    Delete project directory

--- project/src/pages/Group.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import GroupDetail from './GroupDetail';
-
-export default function Group() {
-  return <GroupDetail />;
-} 
--- project/src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Feed from './Feed';
-
-export default function Home() {
-  return <Feed />;
-} 
